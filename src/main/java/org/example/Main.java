3d2cfa35ad3041836ebd9277f4a25e6b4c4a149a
Jameson <j4m350n@testersen.no>
2023-03-16 18:31:55 +0100

    Add instance of each word with own class. *No sane person in enterprise would do otherwise*

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import org.example.words.BazzWord;
+import org.example.words.BuzzWord;
+import org.example.words.FizzWord;
+
 public class Main {
 	public static void main(String[] args) {
 		Runner runner = new Runner();
-		runner.words.add(new Word("Fizz", 3));
-		runner.words.add(new Word("Buzz", 5));
-		runner.words.add(new Word("Bazz", 7));
+		runner.words.add(new FizzWord());
+		runner.words.add(new BuzzWord());
+		runner.words.add(new BazzWord());
 		runner.run(1, 100);
 	}
 }
