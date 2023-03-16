5a59bb80bf4ffe158e453267d48d25e5a7ec4927
Jameson <j4m350n@testersen.no>
2023-03-16 18:09:09 +0100

    Basic traditional implementation of FizzBuzz.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,16 @@ public class Main {
 	}
 
 	public static void run() {
-		for (Integer i = 1; i <= 100; i++) {
-			System.out.println(i);
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(compute(i));
 		}
+	}
+
+	public static String compute(Integer n) {
+		if (n % 3 == 0 && n % 5 != 0) return "Fizz";
+		if (n % 3 != 0 && n % 5 == 0) return "Buzz";
+		// noinspection ConstantValue -- I don't know what IntelliJ has smoked here, but I want one.
+		if (n % 3 == 0 && n % 5 == 0) return "FizzBuzz";
+		return n.toString();
 	}
 }
